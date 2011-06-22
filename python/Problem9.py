2bc3144ce6abd52b0be4430354a038d9ce8d899a
Tim Oram <mitmaro@gmail.com>
2011-06-22 18:37:00 -0230

    Problem 9 Python Solution

---
+++ python/Problem9.py
@@ -0,0 +1,16 @@
+# Problem 9
+# By: Tim Oram
+#
+# Runtime: 4.560s 
+
+import math
+
+for i in xrange(2, 1001):
+	for j in xrange(i+1,1001 - i):
+		for k in xrange(j+1,1001 - i - j):
+			if (i+j+k) != 1000:
+				continue
+			
+			if (math.pow(i,2) + math.pow(j,2)) == math.pow(k,2):
+				print i * j * k
+				break
