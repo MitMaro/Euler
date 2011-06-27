201ce27c120a75db4d579c3dec149947e68b2f26
Tim Oram <mitmaro@gmail.com>
2011-06-27 20:27:09 -0230

    Problem 6 c solution

---
+++ c/Problem006.c
@@ -0,0 +1,19 @@
+// Problem 6
+// By: Tim Oram
+//
+// Runtime: 0.001s
+
+#include <stdio.h>
+
+int main (void) {
+
+	long i;
+	long sum1 = 0, sum2 = 0;
+ 
+	for (i = 1; i <= 100; ++i) {
+		sum1 += i;
+		sum2 += i*i;
+	}
+
+	printf("Answer: %ld\n", sum1*sum1-sum2);
+}
