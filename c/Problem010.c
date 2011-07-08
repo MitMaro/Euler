0c42d8f6c1659f18ae07f34fe7b1b5c06f4ec653
Tim Oram <mitmaro@gmail.com>
2011-07-07 21:54:01 -0230

    Problem 10 c Solution

---
+++ c/Problem010.c
@@ -0,0 +1,43 @@
+// Problem 10
+// By: Tim Oram
+//
+// Runtime: 1.791s
+
+#include <stdio.h>
+
+int main (void) {
+
+	int primes[2000000] = {0};
+
+	int i, j;
+	
+	unsigned long sum = 2;
+
+	// remove 2s
+	for (i = 4; i <= 2000000; i += 2) {
+		primes[i] = 1;
+	}
+
+	i = 3;
+
+	while (i*i <= 2000000) {
+		for (j = i+2; j < 2000000; j += 2) {
+			if (!primes[j] && (j%i) == 0) {
+				primes[j] = 1;
+			}
+		}
+		
+		do {
+			i += 2;
+		} while (primes[i]);
+	}
+
+	for (i = 3; i < 2000000; i += 2) {
+		if (!primes[i]) {
+			sum += i;
+		}
+	}
+
+	printf("Answer: %ld\n", sum);
+	return 0;
+}
