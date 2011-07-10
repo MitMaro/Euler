7e7b9158857fd6cf6489906e611b2a6001474569
Tim Oram <mitmaro@gmail.com>
2011-07-10 16:51:40 -0230

    Problem 25 c Solution

---
+++ c/Problem025.c
@@ -0,0 +1,33 @@
+// Problem 25
+// By: Tim Oram
+//
+// Runtime: 0.001s 
+
+#include <stdio.h>
+
+int main (void) {
+	
+	int i = 2;
+	
+	float a = 1.0, b = 1.0, tmp;
+	
+	int num_digits = 1;
+	
+	while (num_digits < 1000) {
+		
+		tmp = b;
+		b = tmp + a;
+		a = tmp;
+		i++;
+		if (b >= 10) {
+			b /= 10;
+			a /= 10;
+			num_digits++;
+		}
+	}
+	printf("Answer: %d\n", i);
+	return 0;
+}
+
+
+
