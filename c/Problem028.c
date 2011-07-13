a5b5d62c9cbb718d40a637c073cfbe342544b957
Tim Oram <mitmaro@gmail.com>
2011-07-12 23:19:49 -0230

    Problem 28 c Solution

---
+++ c/Problem028.c
@@ -0,0 +1,33 @@
+// Problem 28
+// By: Tim Oram
+//
+// Runtime: 0.001s
+
+#include <stdio.h>
+
+int main (void) {
+	
+	int i = 0;
+	
+	unsigned int j = 1;
+	
+	unsigned int sum = 1;
+	
+	while (i+1 < 1001) {
+		i+= 2;
+		j += i;
+		sum += j;
+		j += i;
+		sum += j;
+		j += i;
+		sum += j;
+		j += i;
+		sum += j;
+	}
+	
+	printf("Answer: %d\n", sum);
+	return 0;
+}
+
+
+
